a41a8cc99082cb99e0163804a66a346bc21fa13a
Alex <misterespia666@yandex.ru>
2018-06-22 11:22:17 +0300

    Update mysql.rb

--- mysql.rb
+++ mysql.rb
@@ -1,18 +1,28 @@
 #!/usr/bin/ruby
 require 'mysql'
 require 'date'
+require 'active_support/all'
 load './sql.rb' #файл с SQL запросами
 require './sql.rb'
-$host='10.124.3.201'
-$user='root'
-$pass='password'
+$host='address'
+$user='user'
+$pass='pass'
 $db = 'zabbix'
 
-
 begin
-    #получаем текущее время
-    $date   =  DateTime.now 
-
+  # опяими передаем за какой день переливать данные. (день за который переливаются данны  = сеголня - d )
+    options = OpenStruct.new
+    OptionParser.new do |opt|
+        opt.on('-d','--days DAYS', "set date current-date - n") { |o| options.days = o }
+    end.parse!
+ 
+        # если опция отсутствует то значение по-умолчанпю - 1
+    unless  options.days.nil?
+        $date   =  DateTime.now - options.days.to_i.day
+    else 
+        $date   =  DateTime.now - 1.day
+    end
+   
     $year   =  $date.strftime("%Y") #извлекаем год
 
     $month  =  $date.strftime("%m") # месяц
@@ -22,8 +32,10 @@ begin
     $dmy    =  $date.strftime("%Y-%m-%d") # а так же строку вида год-месяц-день
       
     con = Mysql.new $host, $user, $pass, $db #получаем объект представляющий подключение согласно заданнмы параметрам
+    # исполняем запросы... ну или просто исполняем :)
     con.query(set_transaction_lock);
-    con.query(move_data) #исполняем запрос... ну или просто исполняем :)
+    con.query (network_discovery)
+    con.query (rvpn)
     
 rescue Mysql::Error => e
     puts e.errno
@@ -32,6 +44,3 @@ rescue Mysql::Error => e
 ensure
     con.close if con
 end
-
-
-
